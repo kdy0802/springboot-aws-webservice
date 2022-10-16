d512d5f2c447d0b07640d12c43372f08ff928c11
kdy0802 <kdy0802@naver.com>
2022-10-16 22:56:20 +0900

    Hello Dto, Controller 추가

---
+++ src/main/java/com/kdy0802/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.kdy0802.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
