aec7304a16fcd685d75c07c46b31ed2775a9b9bb
kdy0802 <kdy0802@naver.com>
2022-11-27 19:21:08 +0900

    feat: Spring Data JPA 테스트 코드 작성하기

---
+++ src/main/java/com/kdy0802/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.kdy0802.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
