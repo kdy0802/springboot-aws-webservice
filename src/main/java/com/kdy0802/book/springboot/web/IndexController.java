0afa02d5a5551f0a1dedf2b423cc162d1749dd95
kdy0802 <kdy0802@naver.com>
2022-12-25 22:40:02 +0900

    feat: 스프링 시큐리티 Oauth2 클라이언트, 구글 서비스 등록, 구글 로그인 연동

--- src/main/java/com/kdy0802/book/springboot/web/IndexController.java
+++ src/main/java/com/kdy0802/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kdy0802.book.springboot.web;
 
+import com.kdy0802.book.springboot.config.oauth.dto.SessionUser;
 import com.kdy0802.book.springboot.domain.posts.PostsService;
 import com.kdy0802.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
   private final PostsService postsService;
+  private final HttpSession httpSession;
 
   @GetMapping("/")
   public String index(Model model) {
     model.addAttribute("posts", postsService.findAllDesc());
+    SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+    if (user != null) {
+      model.addAttribute("userName", user.getName());
+    }
     return "index";
   }
 
