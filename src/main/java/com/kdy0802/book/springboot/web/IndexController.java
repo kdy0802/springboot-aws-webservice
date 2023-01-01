286f6d195670fd1eed209396022c240afd594ab7
kdy0802 <kdy0802@naver.com>
2023-01-01 17:59:25 +0900

    fix: 어노테이션 기반으로 개선하기, 세션 저장소로 데이터베이스 사용하기

--- src/main/java/com/kdy0802/book/springboot/web/IndexController.java
+++ src/main/java/com/kdy0802/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kdy0802.book.springboot.web;
 
+import com.kdy0802.book.springboot.config.oauth.LoginUser;
 import com.kdy0802.book.springboot.config.oauth.dto.SessionUser;
 import com.kdy0802.book.springboot.domain.posts.PostsService;
 import com.kdy0802.book.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
   private final HttpSession httpSession;
 
   @GetMapping("/")
-  public String index(Model model) {
+  public String index(Model model, @LoginUser SessionUser user) {
     model.addAttribute("posts", postsService.findAllDesc());
-    SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
     if (user != null) {
       model.addAttribute("userName", user.getName());
